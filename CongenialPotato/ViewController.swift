a636ad5bfab84afb1c7cd1d8e40ff864a18567a3
CT4TuEI3 <alexstatiev@gmail.com>
2024-07-07 14:38:03 +0300

    Initial commit

---
+++ CongenialPotato/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CongenialPotato
+//
+//  Created by CT4TuEI3 on 07.07.2024.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '15.0'
+
+target 'CongenialPotato' do
+  use_frameworks!
+  
+  pod 'SnapKit'
+  pod 'FirebaseAnalytics'  
+  pod 'FirebaseRemoteConfig'
+  
+end
